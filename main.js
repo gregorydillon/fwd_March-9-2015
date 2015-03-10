7c89cf7dcbc007f0f1bcb8444fab7e89d632b7e7
Gregory Dillon <gregorydillon@gmail.com>
2015-03-09 21:26:46 -0700

    initial commit during class

---
+++ main.js
@@ -0,0 +1,92 @@
+// while (true){
+//   alert('Welcome to my worest website')
+// }
+
+// Numbers
+// Strings
+// Booleans
+// Functions
+alert("get started");
+
+// Arrays
+
+// [1,2,3,4,5];
+// ["Hello", "Goodbye","Get bent"];
+// [1,True,"no",function(){}];
+
+
+// var numbers = [1,2,3,4,5];
+// var greetings = ["Hello", "Goodbye","Get bent"];
+// var dataType = [1,True,"no",function(){}];
+
+
+// ["hello"][0]  ** Confusing example
+
+// and
+// ["hello"][0][0]
+
+
+// for (var i = 0; i < 10; i+=1) {
+//   alert(i);
+// }
+
+
+// for (var i = 0; i < numbers.length; i+=1) {
+//   alert(greetings[i]);
+// }
+
+
+// numbers.push(8);
+
+
+// another data type
+
+// object
+
+// {name:"gregory",age: 58,location:"san francisco"}
+// or more readabile
+// {
+//   name:"gregory",
+//   age: 58,
+//   location:"san francisco",
+//   siblings:"Chris","Tommy",
+// }
+
+// And to do stuff with it, assign it a var
+
+// var gregory =
+//   {
+//   name:"gregory",
+//   age: 58,
+//   location:"san francisco",
+//   siblings:"Chris","Tommy",
+// }
+
+
+// // gregory["name"]
+// // gregory["age"]
+// // gregory[siblings]
+// // gregory[siblings][0]
+
+
+
+// // gregory.name
+// // gregory.age
+// // gregory.siblings
+// // gregory.siblings[0]
+
+
+// gregory.favorite_foods = ["BBQ","Italian"]
+
+// gregory;
+
+// ------
+
+/*Creating functions*/
+
+
+/*var sayHello = function(){
+  alert('hello!!@!@')
+}
+
+sayHello();*/
