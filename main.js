968c8a26d81048613134a219966b92bb58b26858
Gregory Dillon <gregorydillon@gmail.com>
2015-03-10 19:44:09 -0700

    explore use of dot.length within a for statement

--- main.js
+++ main.js
@@ -30,8 +30,8 @@ Run this.
 In the browser, get to console with ctrl-shft-J
 at > numbers
 at > greetings
-at > dataType*/
-
+at > dataType
+*/
 
 // ["hello"][0]  ** Confusing example
 
@@ -42,16 +42,31 @@ at > dataType*/
 
 /*branch 5 explore for statements
 */
-for (var i = 0; i < 10; i+=1) {
+
+/*for (var i = 0; i < 10; i+=1) {
   alert(i);
 }
+*/
 /*end of branch5
 */
 
+/*branch6
+*/
 
-// for (var i = 0; i < numbers.length; i+=1) {
-//   alert(greetings[i]);
-// }
+var numbers = [1,2,3,4,5];
+var greetings = ["Hello", "Goodbye","Get bent"];
+var dataType = [1,true,"no",function(){}];
+
+
+for (var i = 0; i < greetings.length; i+=1) {
+  alert(greetings[i]);
+}
+for (var i = 0; i < numbers.length; i+=1) {
+  alert(numbers[i]);
+}
+
+/*end of branch6
+*/
 
 
 // numbers.push(8);
